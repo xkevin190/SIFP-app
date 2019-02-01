c6c4f856819bf1f34e6a74c6b7c1dbcfbce2dee3
xkevin190 <kevinvelasco193@gmail.com>
2019-02-01 16:13:45 -0400

    lista la funcionalidad del login

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -20,7 +20,7 @@ export default class Equilibrio extends Component {
   constructor(props) {
     super(props)
     this.state = { 
-      view:'register'
+      view:'login'
      }
   }
   handleSubmit= async(values, {resetForm}) =>{
